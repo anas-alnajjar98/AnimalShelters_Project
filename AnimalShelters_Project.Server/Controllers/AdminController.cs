6bac92b2c586e68404414a6e6b70da13d51df060
anas-alnajjar98 <160442902+anas-alnajjar98@users.noreply.github.com>
2024-10-21 16:02:57 +0300

    anas

---
+++ AnimalShelters_Project.Server/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using AnimalShelters_Project.Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalShelters_Project.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+        public AdminController(MyDbContext context)
+        {
+            _context = context;
+            
+        }
+    }
+}
