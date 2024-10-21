cdacd93dac05ac9cedb8e9f46c0db67d39d116fd
dimamohamad <deemaahmad189@gmail.com>
2024-10-21 15:07:45 +0300

    add footer component

--- animalshelters_project.client/src/app/app.module.ts
+++ animalshelters_project.client/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Tuqa/home/home.component';
 import { NavbarComponent } from './Tuqa/navbar/navbar.component';
+import { FooterComponent } from './Tuqa/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
