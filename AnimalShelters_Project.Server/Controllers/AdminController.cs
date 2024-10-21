96e545dd624d07021264aa2beda56a5ac7afbbe1
dimamohamad <deemaahmad189@gmail.com>
2024-10-21 16:22:16 +0300

    addd edit admin

--- AnimalShelters_Project.Server/Controllers/AdminController.cs
+++ AnimalShelters_Project.Server/Controllers/AdminController.cs
@@ -1,6 +1,8 @@
-﻿using AnimalShelters_Project.Server.Models;
+﻿using AnimalShelters_Project.Server.DTOs;
+using AnimalShelters_Project.Server.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
 
 namespace AnimalShelters_Project.Server.Controllers
 {
@@ -26,5 +28,67 @@ namespace AnimalShelters_Project.Server.Controllers
             return NoContent();
         
         }
+
+
+        [HttpGet("AnimalsbyCategoryId/{id}")]
+
+        public IActionResult AnimalsCat(int id)
+        {
+
+            var animals = _context.Animals.Where(a => a.CategoryId == id).ToList();
+
+            if (id <= 0)
+            {
+                return BadRequest();
+
+            }
+            if (animals != null)
+            {
+                return Ok(animals);
+
+            }
+
+            return NotFound();
+
+
+        }
+
+        [HttpPost("addAnimals")]
+        public IActionResult addAnimal([FromForm] AnimalDto animal)
+        {
+
+            var folder = Path.Combine(Directory.GetCurrentDirectory(), "Uploads");
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+            var fileImage = Path.Combine(folder, animal.ImageUrl.FileName);
+            using (var stream = new FileStream(fileImage, FileMode.Create))
+            {
+                animal.ImageUrl.CopyToAsync(stream);
+
+            }
+
+            var newAnimal = new Animal
+            {
+                Name = animal.Name,
+                CategoryId = animal.CategoryId,
+                Breed=animal.Breed,
+                Age=animal.Age,
+                ShelterId= animal.ShelterId,
+                Size = animal.Size,
+                Temperament = animal.Temperament,
+                SpecialNeeds=animal.SpecialNeeds,
+                AdoptionStatus = animal.AdoptionStatus
+            };
+
+            _context.Animals.Add(newAnimal);
+            _context.SaveChanges();
+            return Ok(newAnimal);
+        }
+
+
+
+
     }
 }
