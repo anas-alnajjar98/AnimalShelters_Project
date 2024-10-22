ff19f40648334f5bed7ba83d4c8b01902e841eea
MoFawarah <mfawarah1@gmail.com>
2024-10-22 11:53:24 +0300

    photos are dynamic now (posts)

--- AnimalShelters_Project.Server/Controllers/FawarehController.cs
+++ AnimalShelters_Project.Server/Controllers/FawarehController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AnimalShelters_Project.Server.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AnimalShelters_Project.Server.Controllers
@@ -7,5 +7,57 @@ namespace AnimalShelters_Project.Server.Controllers
     [ApiController]
     public class FawarehController : ControllerBase
     {
+        private readonly MyDbContext _db;
+        public FawarehController(MyDbContext db)
+        {
+            _db = db;
+        }
+
+
+        [HttpPut("AcceptPost/{id}")]
+
+        public IActionResult AcceptPost(int id)
+        {
+            var post = _db.Posts.Where(p => p.Id == id).FirstOrDefault();
+
+            if (post == null)
+            {
+                return NotFound($"No post found with id {id}");
+
+            }
+
+            if (post.Flag == true)
+            {
+                post.Flag = false;
+
+
+            }
+
+            else
+            {
+                post.Flag = true;
+
+            }
+
+            _db.Posts.Update(post);
+            _db.SaveChanges();
+
+
+            return Ok(post);
+        }
+
+        [HttpGet("getImage/{imageName}")]
+        public IActionResult GetImage(string imageName)
+        {
+            var imagePath = Path.Combine(Directory.GetCurrentDirectory(), "Uploads", imageName);
+
+
+            if (System.IO.File.Exists(imagePath))
+            {
+                return PhysicalFile(imagePath, "image/jpeg"); // أو "image/png" حسب نوع الصورة
+            }
+
+            return NotFound();
+        }
     }
 }
